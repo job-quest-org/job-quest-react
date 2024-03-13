2072740120cfa70f6bfa77132b5cf3cfee7c2582
gyanbahadursunuwar <gyanbahadursunuwar@gmail.com>
2024-03-14 00:24:00 +0530

    moved files to root

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <p className="flex font-extrabold text-red-600">Hello world</p>
+        </div>
+    )
+}
+
+export default App
