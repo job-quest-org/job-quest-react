4fc3274ac629c697c5e65494e195d370f2d78e06
jobquestteam <jobquestteam@gmail.com>
2024-03-24 02:50:27 +0530

    Updated OIDC signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react'
 import LoginButton from './authentication/auth'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import './index.css'
 
-const clientId = "361329961613-62a9oo0vdj7on749nft2q4lq8k5p7ep0.apps.googleusercontent.com";
-const res = fetch('http://localhost:5173/api/Candidate/GetCandidateCount', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(response => {
-    // handle response
-  })
-  .catch(error => {
-    console.error('Failed to call API', error);
-  });
 function App() {
     return (
             <div>
                 <p className="flex font-extrabold text-red-600">Hello world</p>
-                
-                <GoogleOAuthProvider clientId={clientId}>
-                    <LoginButton/>
-                </GoogleOAuthProvider>
+                <LoginButton/>
             </div>
             );
         }
--- src/authentication/auth.jsx
+++ src/authentication/auth.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import { useGoogleLogin } from '@react-oauth/google';
 
-function LoginButton() {
-  const googleLogin = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      console.log('Google login successful', tokenResponse);
-    fetch('http://localhost:44396/signin-oidc?code='+{tokenResponse}, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      response.JSON();
-    })
-    .catch(error => {
-      console.error('Failed api', error);
-    });
-      
-    },
-    onError: () => {
-      console.error('Google login failed');
-    },
-    flow: 'auth-code',
-  });
+const LoginButton = () => {
+  const handleLogin = () => {
+    window.location.href = 'https://localhost:44396/login';
+  };
 
   return (
-    <button onClick={() => googleLogin()}>
-      Sign in with Google 
-    </button>
+    <div>
+      <button onClick={handleLogin}>Login</button>
+    </div>
   );
-}
+};
 
 export default LoginButton;
